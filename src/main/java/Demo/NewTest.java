fa90a3d5eb6ef6a76af0d527e7b2ccaeadd6c9f7
AniketGundecha <gundechaaniket05@gmail.com>
2024-06-05 12:17:28 +0530

    First commit from Eclipse

---
+++ src/main/java/Demo/NewTest.java
@@ -0,0 +1,39 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void a() {
+	  System.out.println("Eclipse a");
+  }
+  
+  @Test
+  public void b() {
+	  System.out.println("Eclipse b");
+  }
+  
+  @Test
+  public void c() {
+	  System.out.println("Eclipse c");
+  }
+  
+  @Test
+  public void d() {
+	  System.out.println("Eclipse d");
+  }
+  
+  @Test
+  public void e() {
+	  System.out.println("Eclipse e");
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
