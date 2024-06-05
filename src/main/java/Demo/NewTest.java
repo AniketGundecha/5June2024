57c5d54ce4f5ad71233409bf8b6450f6063bbd6b
AniketGundecha <gundechaaniket05@gmail.com>
2024-06-05 14:19:53 +0530

    Changes from Eclipse

--- src/main/java/Demo/NewTest.java
+++ src/main/java/Demo/NewTest.java
@@ -6,21 +6,23 @@ public class NewTest {
   @Test
   public void a() {
 	  System.out.println("Eclipse a");
+	  System.out.println("Eclipse a2");
   }
   
   @Test
   public void b() {
-	  System.out.println("Eclipse b");
+	  System.out.println("Eclipse b2");
   }
   
   @Test
   public void c() {
-	  System.out.println("Eclipse c");
+	  System.out.println("Eclipse c2");
   }
   
   @Test
   public void d() {
 	  System.out.println("Eclipse d");
+	  System.out.println("Eclipse d2");
   }
   
   @Test
